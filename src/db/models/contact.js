348c1388084a4d84d1c75f4b4bbb43191a44f26c
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-06-09 17:57:28 +0200

    Update contact.js

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -21,7 +21,7 @@ const contactSchema = new Schema(
       type: String,
       validate: emailValidator,
     },
-    isFavourite: {
+    isFavorite: {
       type: Boolean,
       default: false,
     },
