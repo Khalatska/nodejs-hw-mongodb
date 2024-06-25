442ce5d8029df3a3414a574b180dbded735fb61a
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-06-25 18:53:42 +0200

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -48,7 +48,7 @@ export const getContactByIdController = async (req, res) => {
 
 export const createContactController = async (req, res) => {
   const { body, file } = req;
-  const contact = await createContact({ ...body, photo: file }, req.user._id);
+  const contact = await createContact({ ...body, file }, req.user._id);
 
   res.status(201).json({
     status: 201,
@@ -62,7 +62,7 @@ export const patchContactController = async (req, res) => {
   const { contactId } = req.params;
   const result = await updateContact(
     contactId,
-    { ...body, photo: file },
+    { ...body, file },
     req.user._id,
   );
 
