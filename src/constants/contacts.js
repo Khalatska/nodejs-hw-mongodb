be0abe5b05f5f68c70162947b4bafbed9c84390a
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-07-03 11:11:24 +0200

    added delete and patch endpoints

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -26,3 +26,5 @@ export const CLOUDINARY = {
 };
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'src', 'temp');
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
